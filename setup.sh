49369f811904b95a04912faa4e7eef86217e4121
MOHIT MALANI <32036961+mohitmalani@users.noreply.github.com>
2023-02-13 17:22:27 -0500

    Assignment_4

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo 'Updating Packages'
+sudo yum update -y
+
+echo 'cleaning cache'
+sudo yum clean all
+sudo yum makecache
+echo 'installing Open jdk 8'
+cd ~
+sudo yum install wget -y
+sleep 10
+sudo yum install java-1.8.0-openjdk -y
+sleep 10
+java -version
+echo 'installed java successfully.'
+
+
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo rpm -Uvh mysql80-community-release-el7-3.noarch.rpm
+sudo yum install mysql-server -y
+echo "sleeping....."
+sleep 10
+sudo systemctl start mysqld.service
+sudo systemctl status mysqld.service
+echo "sleeping....."
+sleep 10
+pwd=`sudo grep 'temporary password' /var/log/mysqld.log | rev | cut -d':' -f 1 | rev | xargs`
+mysql -uroot -p$pwd --connect-expired-password -e "Alter user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Masters@2022'"
+mysql -uroot -pMasters@2022  -e "CREATE DATABASE IF NOT EXISTS user_db"
+
+pwd
+sudo chmod 755 health-check-api.service
+sudo chmod 755 health-check-api-0.0.1-SNAPSHOT.jar
+sudo mkdir webservice
+sudo mv health-check-api-0.0.1-SNAPSHOT.jar webservice/
+
+sudo mv health-check-api.service /etc/systemd/system/health-check-api.service
+
+sudo systemctl start health-check-api.service
+sudo systemctl enable health-check-api.service
+sudo systemctl status health-check-api.service
